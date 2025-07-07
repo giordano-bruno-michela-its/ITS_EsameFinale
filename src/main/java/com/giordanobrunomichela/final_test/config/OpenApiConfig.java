52b5513c06e809ea8e844bf933a1e8f52b87eba6
Giordano Bruno Michela <giordano.michela@edu.itspiemonte.it>
2025-07-07 13:54:36 +0200

    Removed Spring Security

--- src/main/java/com/giordanobrunomichela/final_test/config/OpenApiConfig.java
+++ src/main/java/com/giordanobrunomichela/final_test/config/OpenApiConfig.java
@@ -2,10 +2,7 @@ package com.giordanobrunomichela.final_test.config;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,14 +10,8 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "Agora Backend API", version = "v1"),
-        security = @SecurityRequirement(name = "bearerAuth")
+        info = @Info(title = "Final Test API", version = "v1")
 )
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
+
 public class OpenApiConfig {
 }
--- src/main/java/com/giordanobrunomichela/final_test/dto/UpdatePasswordDTO.java
+++
@@ -1,9 +0,0 @@
-package com.giordanobrunomichela.final_test.dto;
-
-import lombok.Data;
-
-@Data
-public class UpdatePasswordDTO {
-    private String oldPassword;
-    private String newPassword;
-}
