2fd3074b6203d165d8a0e7d2bac4f7419951a73f
Maria Doro <161697452+mariadoro0@users.noreply.github.com>
2025-07-07 09:56:27 +0200

    Start 1

---
+++ src/main/java/com/giordanobrunomichela/final_test/model/PhoneNumber.java
@@ -0,0 +1,26 @@
+package com.giordanobrunomichela.final_test.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class PhoneNumber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String description;
+    private String number;
+    
+}
